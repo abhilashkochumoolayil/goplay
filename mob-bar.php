5bb5f4aa5ed2fe47eedfc76651d39949b7a553b4
Abhilash <abhi4tec@gmail.com>
2024-11-13 20:20:18 +0530

    .php file updated to html

--- mob-bar.php
+++ mob-bar.php
@@ -4,11 +4,11 @@
         <nav class="menu_main_nav_area">
             <ul id="menu_main" class="menu_main_nav">
 
-                <li class="menu-item"><a href="index.php"><span>Home</span></a></li>
-                <li class="menu-item"><a href="about-us.php"><span>About Us</span></a></li>
-                <!-- <li class="menu-item"><a href="event.php"><span>Event</span></a></li> -->
-                <li class="menu-item"><a href="gallery.php"><span>Gallery</span></a></li>
-                <li class="menu-item"><a href="contact.php"><span>Contact</span></a></li>
+                <li class="menu-item"><a href="index.html"><span>Home</span></a></li>
+                <li class="menu-item"><a href="about-us.html"><span>About Us</span></a></li>
+                <!-- <li class="menu-item"><a href="event.html"><span>Event</span></a></li> -->
+                <li class="menu-item"><a href="gallery.html"><span>Gallery</span></a></li>
+                <li class="menu-item"><a href="contact.html"><span>Contact</span></a></li>
             </ul>
         </nav>
 
